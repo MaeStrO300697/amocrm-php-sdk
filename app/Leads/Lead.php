365dbf684a612104f937c5b88ad69a58ea57db1d
MaeStrO300697 <artem.kar18@gmail.com>
2020-05-26 16:29:05 +0300

    v1

---
+++ app/Leads/Lead.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 26.05.2020
+ * Time: 12:07
+ */
+
+namespace maestro300697\amocrmSdk\Leads;
+
+
+class Lead
+{
+    /**
+     * Название сделки
+     *
+     * @var string
+     */
+    public $name;
+    /**
+     * Дата создания текущей сделки
+     *
+     * @var
+     */
+    public $created_at;
+    /**
+     * @var
+     */
+    public $status_id; // Дата изменения текущей сделки
+    /**
+     * @var
+     */
+    public $sale; // Бюджет сделки
+    /**
+     * @var
+     */
+    public $responsible_user_id; //ID ответственного пользователя
+    /**
+     * @var
+     */
+    public $tags;
+    /**
+     * @var
+     */
+    public $contacts_id; //Уникальный идентификатор контакта, для связи с сделкой.
+    /**
+     * @var
+     */
+    public $custom_fields; // Внутри данного массива находится содержимое каждого заполненного дополнительного поля
+
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once 'vendor/autoload.php';
+
+$class = new \maestro300697\amocrmSdk\queryGenerator('jackroyal2020256','cd14a4a4bc47a07ac5053df58c5301cf9d68243c');
+
+echo $class->getSubdomain();
