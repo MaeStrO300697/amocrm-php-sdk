de6e8a4a31236efeff787fd1f1d9fda5b9b57c90
MaeStrO300697 <artem.kar18@gmail.com>
2020-05-27 11:06:03 +0300

    travis ci

--- app/auth.php
+++ app/auth.php
@@ -20,10 +20,12 @@ curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($user));
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_COOKIEFILE, dirname
-    (__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
-curl_setopt($curl, CURLOPT_COOKIEJAR, dirname
-    (__FILE__) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl, CURLOPT_COOKIEFILE, dirname(
+    __FILE__
+) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
+curl_setopt($curl, CURLOPT_COOKIEJAR, dirname(
+    __FILE__
+) . '/cookie.txt'); #PHP>5.3.6 dirname(__FILE__) -> __DIR__
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
 $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
@@ -44,13 +46,11 @@ $errors = array(
     502 => 'Bad gateway',
     503 => 'Service unavailable',
 );
-try
-{
+try {
     #Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
     if ($code != 200 && $code != 204) {
         throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
     }
-
 } catch (Exception $E) {
     die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
 }
@@ -60,8 +60,7 @@ try
  */
 $Response = json_decode($out, true);
 $Response = $Response['response'];
-if (isset($Response['auth'])) #Флаг авторизации доступен в свойстве "auth"
-{
+if (isset($Response['auth'])) { #Флаг авторизации доступен в свойстве "auth"
     echo 'Авторизация прошла успешно';
     exit;
 }
